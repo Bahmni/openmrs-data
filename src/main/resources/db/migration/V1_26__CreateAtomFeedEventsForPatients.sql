c5af6c3a5a3b3283f2fa0bd6b341cd47569a5773
pchandra <pchandra@thoughtworks.com>
2013-08-14 11:17:31 +0530

    praveen,d3|updating changed date to created date for all patients

--- src/main/resources/db/migration/V1_26__CreateAtomFeedEventsForPatients.sql
+++ src/main/resources/db/migration/V1_26__CreateAtomFeedEventsForPatients.sql
@@ -1,3 +1,5 @@
+update patient set date_changed = date_created,changed_by = creator where date_changed is null ;
+
 INSERT INTO event_records (uuid, title, object, category)
   SELECT person.uuid, 'Patient', concat('/openmrs/ws/rest/v1/patient/', person.uuid, '?v=full'), 'patient'
-  FROM person JOIN patient on person_id = patient_id;
+  FROM person JOIN patient on person_id = patient_id order by patient.date_changed asc;

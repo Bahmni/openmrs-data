39ff9ee07a08f158e6411c6594db4077ba4c39c3
indraneelr <indraneel.rr@gmail.com>
2013-08-21 16:59:46 +0530

    Indraneel | adding new script to correct sort order of person attribute type

---
+++ src/main/resources/db/migration/V1_47__RegistrationPersonAttributeSortOrderUpdate.sql
@@ -0,0 +1,12 @@
+update person_attribute_type set sort_weight = 2 where name='caste';
+update person_attribute_type set sort_weight = 3 where name='class';
+update person_attribute_type set sort_weight = 4 where name='education';
+update person_attribute_type set sort_weight = 5 where name='occupation';
+update person_attribute_type set sort_weight = 6 where name='primaryContact';
+update person_attribute_type set sort_weight = 7 where name='secondaryContact';
+update person_attribute_type set sort_weight = 8 where name='primaryRelative';
+update person_attribute_type set sort_weight = 9 where name='secondaryIdentifier';
+update person_attribute_type set sort_weight = 1.1 where name='healthCenter';
+update person_attribute_type set sort_weight = 1.2 where name='familyNameLocal';
+update person_attribute_type set sort_weight = 1.3 where name='givenNameLocal';
+

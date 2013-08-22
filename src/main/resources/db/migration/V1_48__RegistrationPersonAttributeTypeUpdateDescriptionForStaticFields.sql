28cd54d766cd50c72eea6bd3e25cb4840d16b15a
indraneelr <indraneel.rr@gmail.com>
2013-08-22 12:18:03 +0530

    indraneel | updating the description for the static fields(healthcenter,localname etc) in registration

---
+++ src/main/resources/db/migration/V1_48__RegistrationPersonAttributeTypeUpdateDescriptionForStaticFields.sql
@@ -0,0 +1,3 @@
+update person_attribute_type set description = CONCAT(description,'. Static field. Ordering not configurable.') where name='healthCenter';
+update person_attribute_type set description = CONCAT(description,'. Static field. Ordering not configurable.') where name='familyNameLocal';
+update person_attribute_type set description = CONCAT(description,'. Static field. Ordering not configurable.') where name='givenNameLocal';

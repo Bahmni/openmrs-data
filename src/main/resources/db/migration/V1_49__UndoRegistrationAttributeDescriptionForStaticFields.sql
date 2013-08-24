0e018e8985ad76df8045184fec96051ace87a281
indraneelr <indraneel.rr@gmail.com>
2013-08-24 12:33:46 +0530

    Indraneel | setting the description for static fields on registration back to what it was

---
+++ src/main/resources/db/migration/V1_49__UndoRegistrationAttributeDescriptionForStaticFields.sql
@@ -0,0 +1,3 @@
+UPDATE person_attribute_type SET description = '????? ?? ???' WHERE name = 'givenNameLocal';
+UPDATE person_attribute_type SET description = '????? ?? ?????' WHERE name = 'familyNameLocal';
+UPDATE person_attribute_type SET description = 'Center' WHERE name = 'healthCenter';

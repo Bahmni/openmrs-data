7193707fbf8da5912cdaeb9dc9e9cf6f6a36ddb4
Deepak N <endeep123@gmail.com>
2013-09-02 17:51:52 +0530

    Fixing migration 49

--- src/main/resources/db/migration/V1_49__UndoRegistrationAttributeDescriptionForStaticFields.sql
+++ src/main/resources/db/migration/V1_49__UndoRegistrationAttributeDescriptionForStaticFields.sql
@@ -1,3 +1,3 @@
-UPDATE person_attribute_type SET description = '????? ?? ???' WHERE name = 'givenNameLocal';
-UPDATE person_attribute_type SET description = '????? ?? ?????' WHERE name = 'familyNameLocal';
+UPDATE person_attribute_type SET description = 'मरीज़ का नाम' WHERE name = 'givenNameLocal';
+UPDATE person_attribute_type SET description = 'मरीज़ का उपनाम' WHERE name = 'familyNameLocal';
 UPDATE person_attribute_type SET description = 'Center' WHERE name = 'healthCenter';

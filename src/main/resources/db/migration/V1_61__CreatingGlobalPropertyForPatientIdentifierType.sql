ce43d3ca0e19cdf87699b6805abfedd63829c947
Mário Areias <mario.s.s.areias@gmail.com>
2013-10-04 11:55:14 +0530

    fixing script about global property

--- src/main/resources/db/migration/V1_61__CreatingGlobalPropertyForPatientIdentifierType.sql
+++ src/main/resources/db/migration/V1_61__CreatingGlobalPropertyForPatientIdentifierType.sql
@@ -1,7 +1,6 @@
 -- JSS specific. Do not move to registration omod
 
-INSERT INTO global_property (property,property_value,description,uuid) 
-VALUES ('emr.primaryIdentifierType','05a29f94-c0ed-11e2-94be-8c13b969e334','Primary identifier type for looking up patients, generating barcodes, etc','a18e17c8-8557-434d-bbfc-24bf25432fb3');
 
+UPDATE global_property SET value = '05a29f94-c0ed-11e2-94be-8c13b969e334' WHERE property='emr.primaryIdentifierType';
 
 UPDATE patient_identifier_type SET uuid='05a29f94-c0ed-11e2-94be-8c13b969e334' WHERE name = 'JSS';

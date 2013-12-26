c297a45ce092f4634a5b584019ce7e2c1604e6c7
pchandra <pchandra@thoughtworks.com>
2013-12-26 15:21:22 +0530

    praveen|adding global properties for encounter matcher and session duration

---
+++ src/main/resources/db/migration/V1_82__EncounterSession_Duration.sql
@@ -0,0 +1,8 @@
+insert into global_property (`property`, `property_value`, `description`, `uuid`)
+values ('bahmni.encountersession.duration',
+			'60',
+			'Encountersession duration in minutes',
+			uuid()
+		);
+
+
